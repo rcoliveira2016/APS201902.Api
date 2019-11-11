b71024ad9e847e44eca36a572663829d2a89966c
rcoliveira2016 <roramon7@gmail.com>
2019-11-10 22:44:48 -0300

    email

--- enviar-email.php
+++ enviar-email.php
@@ -3,10 +3,24 @@ $keyApi = 'at_8oIJWGMuZTOaiEQN8K6O52JtcgQ6j';
 $urlApi = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=$keyApi&domainName=";
 
 $url = $_POST['url'];
-echo $url;
 $domain = parse_url($url, PHP_URL_HOST);
 
 $xml = file_get_contents("$urlApi$domain");
 $whois = new SimpleXMLElement($xml);
 $emailContato = $whois->contactEmail;
+
+// emails para quem será enviado o formulário
+  $emailenviar = "roramon7@gmail.com";
+  $destino = $emailenviar;
+  $assunto = "Contato pelo Site";
+ 
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
+   
+  $enviaremail = mail($destino, $assunto, null, $headers);
+
+echo $emailContato;
 ?>
