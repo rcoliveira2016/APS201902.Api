d6717c4ac6834def8106d4aa60142a80e25112c8
rcoliveira2016 <roramon7@gmail.com>
2019-11-12 19:35:24 -0300

    texto email

--- montar-email.php
+++ montar-email.php
@@ -1,5 +1,5 @@
 <?php  
-function teste(){
+function MontarEmail(){
 	ob_start();
 	$titulo = "LGPD Helper";
 ?>
@@ -12,7 +12,10 @@ function teste(){
 	</head>
 	<body style="margin: 0; padding: 0;">
 		<h1><?= $titulo; ?></h1>
-		<p></p>
+		<p>Esse e-mail foi gerado pela extenção <b><?= $titulo; ?></b></p>
+		<p>A extenção tem como objetivo ajudar os administradores e devenvolvedores de sistemas, a se adequar as normas da <a href="http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/L13709.htm">Lei Geral de Proteção de Dados Pessoais (LGPDP)</a>, lei de  nº 13.709/2018</p>
+		<p>A LGPD, é a legislação brasileira que regula as atividades de tratamento de dados pessoais e que também altera os artigos 7º e 16 do Marco Civil da Interne</p>
+				
 	</body>
 </html>
 <?php
@@ -21,5 +24,5 @@ function teste(){
   return  $pagemaincontent;
 }
   
-  echo teste();
+  echo MontarEmail();
 ?>
